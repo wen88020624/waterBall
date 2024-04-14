62a3884c4070e554443f09648c6bc4f3512eca66
wen88020624 <wen880206@gmail.com>
2024-04-14 15:22:23 +0800

    新增HabitBasedReverse及其單元測試

--- matchmakingSystem/Code/src/main/test/HabitBasedTest.java
+++ matchmakingSystem/Code/src/main/test/HabitBasedTest.java
@@ -29,4 +29,28 @@ public class HabitBasedTest {
         Exception exception =  assertThrowsExactly(RuntimeException.class, system::match);
         assertEquals("No match found for individual!", exception.getMessage());
     }
+
+    @Test
+    void testMatchByHabitBasedReverse() {
+        var system = new MatchmakingSystem(new HabitBasedReverse());
+        var individual1 = new Individual(Gender.MALE, 25, "Hi, I'm A.", "coding,reading,dancing", new Coord(0, 0), system);
+        var individual2 = new Individual(Gender.FEMALE, 23, "Hi, I'm B.", "jumping,jogging", new Coord(0, 0), system);
+        var individual3 = new Individual(Gender.FEMALE, 21, "Hi, I'm C.", "swimming,hiking", new Coord(0, 0), system);
+        var individual4 = new Individual(Gender.FEMALE, 21, "Hi, I'm D.", "coding,reading,music,traveling", new Coord(0, 0), system);
+
+        system.match();
+
+        assertEquals(individual1.getMatchWith(), individual2);
+        assertEquals(individual2.getMatchWith(), individual1);
+        assertEquals(individual3.getMatchWith(), individual1);
+        assertEquals(individual4.getMatchWith(), individual2);
+    }
+
+    @Test
+    void testNoMatchFoundExceptionByHabitBasedReverse() {
+        var system = new MatchmakingSystem(new HabitBased());
+        var individual1 = new Individual(Gender.MALE, 25, "Hi, I'm A.", "coding,reading,dancing", new Coord(0, 0), system);
+        Exception exception =  assertThrowsExactly(RuntimeException.class, system::match);
+        assertEquals("No match found for individual!", exception.getMessage());
+    }
 }
