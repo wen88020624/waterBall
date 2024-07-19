47708fae77573679830999eb77dbce300bffe00a
Wen <2300728@systex.com.tw>
2024-07-19 16:52:28 +0800

    Refactor Rank

--- chapter1/1.H/OOP/showDownV2/src/main/java/org/example/Card.java
+++ chapter1/1.H/OOP/showDownV2/src/main/java/org/example/Card.java
@@ -4,10 +4,6 @@ public class Card {
     private final Rank rank;
     private final Suit suit;
 
-    public enum Rank {
-        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JAKE, QUEEN, KING, ACE
-    }
-
     public enum ShowDownResult {
         BIGGER, SMALLER
     }
@@ -24,6 +20,26 @@ public class Card {
         @Override
         public String toString() {
             return String.valueOf(representation);
+        }
+    }
+
+    public enum Rank implements Comparable<Rank>{
+        R2("2"),
+        R3("3"), R4("4"), R5("5"),
+        R6("6"), R7("7"), R8("8"),
+        R9("9"), R10("10"),
+        J("J"), Q("Q"), K("K"),
+        A("A");
+
+        private final String representation;
+
+        Rank(String representation) {
+            this.representation = representation;
+        }
+
+        @Override
+        public String toString() {
+            return representation;
         }
     }
 
